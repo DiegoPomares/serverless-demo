d17d65330eb71393e1d83aa469cd9665c7de68fe
Diego Pomares <github@diegopomares.com>
2018-10-15 16:36:07 +0200

    Refactor

--- templates/api_gateway.py
+++ templates/api_gateway.py
@@ -5,110 +5,130 @@ from troposphere import Template, Parameter, Ref, Output, GetAtt, Join
 from troposphere.awslambda import Function, Code, Version
 from troposphere.apigateway import Deployment, Stage, RestApi
 from troposphere.iam import Role, Policy
+from troposphere.cloudformation import AWSCustomObject
 
-DEFAULTS = {
-    'LambdaNames': ["Hello"]
-}
+SWAGGER_TEMPLATE_PATH = "../src/swagger.cloudformation.yaml"
 MAPPING_TEMPLATE = '#set($allParams=$input.params()){"body-json":$input.json("$"),"params":{#foreach($type in $allParams.keySet())#set($params=$allParams.get($type)) "$type":{#foreach($paramName in $params.keySet())"$paramName":"$util.escapeJavaScript($params.get($paramName))"#if($foreach.hasNext),#end#end}#if($foreach.hasNext),#end#end},"stage-variables":{#foreach($key in $stageVariables.keySet())"$key":"$util.escapeJavaScript($stageVariables.get($key))"#if($foreach.hasNext),#end#end}}'
-
-template = Template()
-
-
-def sceptre_handler(_sceptre_user_data=None):
-    return template.to_yaml()
+CUSTOM_APIGW_DEPLOYMENT_PATH = "../custom_resources/apigw_deployment.py"
 
 
-lambda_parameters = {}
+def sceptre_handler(sceptre_user_data=None):
+    sceptre_user_data = sceptre_user_data or {}
 
-def ref_constructor(loader, node):
-    element = loader.construct_scalar(node)
-    return Ref(element)
-
-def getatt_constructor(loader, node):
-    element, attribute = loader.construct_scalar(node).split('.')
-    return GetAtt(element, attribute)
-
-yaml.add_constructor('!Ref', ref_constructor)
-yaml.add_constructor('!GetAtt', getatt_constructor)
-
-script_path = os.path.dirname(os.path.realpath(__file__))
-swagger_path = os.path.join(script_path, '../src/swagger.cloudformation.yaml')
-with open(swagger_path) as f:
-    swagger = yaml.load(f.read())
-
-json_mapping_template = template.add_parameter(Parameter(
-    "MappingTemplate",
-    Description="Mapping template for request body, uri and body params, and stage variables",
-    Default=MAPPING_TEMPLATE,
-    Type="String"
-))
+    template = MainTemplate(sceptre_user_data.get('LambdaFunctionNames', []))
+    yaml = template.t.to_yaml()
+    return yaml
 
 
-def add_lambda_uri_parameters(name):
-    parameter_name = f"{name}LambdaURI"
-    lambda_parameters[parameter_name] = template.add_parameter(Parameter(
+class CustomLambdaVersion(AWSCustomObject):
+    resource_type = "Custom::LambdaVersion"
+
+    props = {
+        'ServiceToken': (str, True),
+        'FunctionName': (str, True),
+        'S3ObjectVersion': (str, True),
+    }
+
+
+class MainTemplate:
+    def __init__(self, lambda_names):
+        self.t = Template()
+
+        json_mapping_template = self.t.add_parameter(Parameter(
+            "MappingTemplate",
+            Description="Mapping template for request body, uri and body params, and stage variables",
+            Default=MAPPING_TEMPLATE,
+            Type="String"
+        ))
+
+        for l in lambda_names:
+            self.add_lambda_uri_parameters(l)
+
+        apigw_role = self.t.add_resource(Role(
+            "APIGWRole",
+            AssumeRolePolicyDocument={
+                "Version": "2012-10-17",
+                "Statement": [{
+                    "Action": ["sts:AssumeRole"],
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": ["apigateway.amazonaws.com"]
+                    }
+                }]
+            },
+            Policies=[Policy(
+                PolicyName="APIGateway",
+                PolicyDocument={
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Effect": "Allow",
+                            "Action": ["lambda:InvokeFunction", "iam:PassRole"],
+                            "Resource": "*",
+                        },
+                    ]
+                })
+            ],
+        ))
+
+        apigw = self.t.add_resource(RestApi(
+            "APIGW",
+            Body=self.get_swagger()
+        ))
+
+        apigw_deployment = self.t.add_resource(Deployment(
+            "APIGWDeployment",
+            RestApiId=Ref(apigw)
+        ))
+
+        apigw_stage_latest = self.t.add_resource(Stage(
+            "APIGWStage",
+            StageName='latest',
+            RestApiId=Ref(apigw),
+            DeploymentId=Ref(apigw_deployment)
+        ))
+
+        self.t.add_output(Output(
+            "URL",
+            Value=Join('', [
+                'https://',
+                Ref(apigw),
+                '.execute-api.',
+                Ref('AWS::Region'),
+                '.amazonaws.com/',
+                Ref(apigw_stage_latest),
+            ]),
+            Description="API Gateway URL, 'latest' stage"
+        ))
+
+    def add_lambda_uri_parameters(self, name):
+        parameter_name = f"{name}LambdaURI"
+        self.t.add_parameter(Parameter(
             parameter_name,
             Type="String"
     ))
 
+    def get_swagger(self):
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        swagger_path = os.path.join(script_path, SWAGGER_TEMPLATE_PATH)
 
-for l in DEFAULTS['LambdaNames']:
-    add_lambda_uri_parameters(l)
+        def ref_constructor(loader, node):
+            element = loader.construct_scalar(node)
+            return Ref(element)
 
-apigw_role = template.add_resource(Role(
-    "APIGWRole",
-    AssumeRolePolicyDocument={
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Action": ["sts:AssumeRole"],
-            "Effect": "Allow",
-            "Principal": {
-                "Service": ["apigateway.amazonaws.com"]
-            }
-        }]
-    },
-    Policies=[Policy(
-        PolicyName="APIGateway",
-        PolicyDocument={
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Effect": "Allow",
-                    "Action": ["lambda:InvokeFunction", "iam:PassRole"],
-                    "Resource": "*",
-                },
-            ]
-        })
-    ],
-))
+        def getatt_constructor(loader, node):
+            element, attribute = loader.construct_scalar(node).split('.')
+            return GetAtt(element, attribute)
 
+        yaml.add_constructor('!Ref', ref_constructor)
+        yaml.add_constructor('!GetAtt', getatt_constructor)
 
-apigw = template.add_resource(RestApi(
-    "APIGW",
-    Body=swagger
-))
+        with open(swagger_path) as f:
+            return yaml.load(f.read())
 
-apigw_deployment = template.add_resource(Deployment(
-    "APIGWDeployment",
-    RestApiId=Ref(apigw)
-))
+    def get_custom_apigw_deployment_code(self):
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        code_path = os.path.join(script_path, CUSTOM_APIGW_DEPLOYMENT_PATH)
 
-apigw_stage_latest = template.add_resource(Stage(
-    "APIGWStage",
-    StageName='latest',
-    RestApiId=Ref(apigw),
-    DeploymentId=Ref(apigw_deployment)
-))
-
-template.add_output(Output(
-    "URL",
-    Value=Join('', [
-        'https://',
-        Ref(apigw),
-        '.execute-api.',
-        Ref('AWS::Region'),
-        '.amazonaws.com/',
-        Ref(apigw_stage_latest),
-    ]),
-    Description="API Gateway URL, 'latest' stage"
-))
+        with open(code_path) as f:
+            return f.read()
